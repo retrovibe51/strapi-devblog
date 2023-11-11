6e81a9ed2f138bb7e4449a82a01947c0fd13fcfd
weinschel <weinschel@smartshore.nl>
2023-11-11 17:26:43 +0530

    added public_server_url for server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,3 @@
 module.exports = ({env}) => ({
-    url: env("RENDER_EXTERNAL_URL", "")
+    url: env("PUBLIC_SERVER_URL", "")     // replaced by earlier value: env("RENDER_EXTERNAL_URL", "") which used to get set automatically by render.com site as env variable
 })
