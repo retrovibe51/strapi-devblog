608894aaff6394af769b3c6e57a2ed2d29c1e829
weinschel <weinschel@smartshore.nl>
2023-11-14 12:21:24 +0530

    corrections  in email config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,20 +1,22 @@
 module.exports = ({ env }) => ({
     email: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'smtp.example.com'),
-        port: env('SMTP_PORT', 587),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.example.com'),
+          port: env('SMTP_PORT', 587),
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          // ... any custom nodemailer options
+          secure: true
         },
-        // ... any custom nodemailer options
-        secure: true
-      },
-      settings: {
-        defaultFrom: 'weinschel@smartshore.nl',
-        defaultReplyTo: 'weinschel@smartshore.nl',
-      },
+        settings: {
+          defaultFrom: 'weinschel@smartshore.nl',
+          defaultReplyTo: 'weinschel@smartshore.nl',
+        },
+      }
     },
     
     upload: {
